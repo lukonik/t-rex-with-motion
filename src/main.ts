169934e35814b869afbdb8efa2a1d0ef4f4e8ae5
luka onik <luka.onikadze@toptal.com>
2024-07-16 15:54:31 +0400

    wait for styles

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import "@lottiefiles/lottie-player";
 import adapter from 'webrtc-adapter';
-
-bootstrapApplication(AppComponent, appConfig)
+window.addEventListener('load', (event) => {
+  bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+});
